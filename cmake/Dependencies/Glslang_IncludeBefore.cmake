5ed67940d6dee1af135e9043492d192942a84ed6
Julien Combattelli <julien.combattelli@gmail.com>
2024-08-15 19:13:21 +0200

    Make the project cross-platform and cross-compilable with MinGW

--- cmake/Dependencies/Glslang_IncludeBefore.cmake
+++ cmake/Dependencies/Glslang_IncludeBefore.cmake
@@ -1,16 +1,17 @@
 # COMMAND_ERROR_IS_FATAL was added in CMake 3.19
 cmake_minimum_required(VERSION 3.19)
 
-message(CHECK_START "Downloading Shaderc dependencies")
+message(CHECK_START "Downloading Glslang dependencies")
 
 find_package(Python3 COMPONENTS Interpreter REQUIRED)
 
 execute_process(
-    COMMAND ${Python3_EXECUTABLE} utils/git-sync-deps
-    WORKING_DIRECTORY ${shaderc_SOURCE_DIR}
+    COMMAND ${Python3_EXECUTABLE} update_glslang_sources.py
+    WORKING_DIRECTORY ${glslang_SOURCE_DIR}
     COMMAND_ECHO STDERR
     COMMAND_ERROR_IS_FATAL ANY
     OUTPUT_QUIET
+    ERROR_QUIET
 )
 
 # Fail pass is handled with COMMAND_ERROR_IS_FATAL
